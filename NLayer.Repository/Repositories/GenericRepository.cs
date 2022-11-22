b753e0320dcaad9dbb56ccf8049b839f248a36ac
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2022-11-22 13:47:44 +0300

    Service Added Generic

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -30,12 +30,12 @@ namespace NLayer.Repository.Repositories
             await _dbSet.AddRangeAsync(entities);
         }
 
-        public async Task<bool> Any(Expression<Func<T, bool>> expression)
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
         {
             return await _dbSet.AnyAsync(expression);
         }
 
-        public  IQueryable<T> GetAll(Expression<Func<T, bool>> expression)
+        public  IQueryable<T> GetAll()
         {
             return  _dbSet.AsNoTracking().AsQueryable();
         }
@@ -50,7 +50,7 @@ namespace NLayer.Repository.Repositories
             _dbSet.Remove(entity);
         }
 
-        public void RemoveRangeAsync(IEnumerable<T> entities)
+        public void RemoveRange(IEnumerable<T> entities)
         {
             _dbSet.RemoveRange(entities);
         }
