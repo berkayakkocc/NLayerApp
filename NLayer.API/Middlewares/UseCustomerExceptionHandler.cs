8f9d161ea493fd1bd4b4034cca8021a5be67c5bb
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2022-12-03 17:36:39 +0300

    Global Exception Handler Added

---
+++ NLayer.API/Middlewares/UseCustomerExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using NLayer.Core.Dtos;
+using NLayer.Service.Exceptions;
+using System.Text.Json;
+
+namespace NLayer.API.Middlewares
+{
+    public static class UseCustomerExceptionHandler
+    {
+        public static void UserCustomExpection(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        ClientSideException => 400,
+                        _ => 500
+                    };
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                });
+            });
+        }
+    }
+}
