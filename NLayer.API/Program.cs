26b429c5748e8a8bfc8a41b15db86bb35f7ecfcc
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2023-01-08 15:05:09 +0300

    Caching Added

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -33,6 +33,8 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddMemoryCache();
 builder.Services.AddScoped(typeof(NotFoundFilter<>));
 
 
