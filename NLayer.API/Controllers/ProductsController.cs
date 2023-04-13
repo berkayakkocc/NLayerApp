32bacec9426950e5dbf3bb14fee1282c1cb9044a
Berkaycan Akkoç <berkayakkocc6@gmail.com>
2023-04-14 00:32:38 +0300

    Refactor Controller

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -10,22 +10,23 @@ namespace NLayer.API.Controllers
     [ApiController]
     public class ProductsController : CustomBaseController
     {
-        private readonly IService<Product> _service;
         private readonly IMapper _mapper;
-        private readonly IProductService _productService;
+        private readonly IProductService _service;
+     
 
-        public ProductsController(IService<Product> service, IMapper mapper, IProductService productService)
+        public ProductsController(IService<Product> service, IMapper mapper,IProductService productService )
         {
-            _service = service;
+           
             _mapper = mapper;
-            _productService = productService;
+            _service = productService;
+         
         }
 
         /// GET api/products/GetProductsWithCategory
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory()
         {
-            return CreateActionResult(await _productService.GetProductsWithCategory());
+            return CreateActionResult(await _service.GetProductsWithCategory());
         }
 
         /// GET api/products
