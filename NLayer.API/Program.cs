5ad24b04e69965b94008e76f9779e0aa740d1b1e
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-04-13 16:15:30 +0300

    Automapper added with project

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using NLayer.Core.Repositories;
+using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
+using NLayer.Service;
+using NLayer.Service.Mapping;
 using System.Reflection;
 
 namespace NLayer.API
@@ -25,7 +28,9 @@ namespace NLayer.API
 
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            //builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+            builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+
+            builder.Services.AddAutoMapper(typeof(MapProfile));
 
             builder.Services.AddDbContext<AppDbContext>(x =>
             {
