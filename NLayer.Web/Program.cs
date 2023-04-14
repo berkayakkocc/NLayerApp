9ff91b0d57b210f3ea869eea1acc3053b55c5316
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-04-14 15:49:40 +0300

    Default error view model deleted with added to custom error view model

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -28,6 +28,7 @@ namespace NLayer.Web
                 });
             });
 
+            builder.Services.AddScoped(typeof(NotFoundFilter<>));
             builder.Host.UseServiceProviderFactory
           (new AutofacServiceProviderFactory());
             builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
@@ -37,7 +38,7 @@ namespace NLayer.Web
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+               
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
