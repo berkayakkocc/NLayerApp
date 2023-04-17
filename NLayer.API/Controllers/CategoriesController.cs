7e1cb025c1ab0fa1e6b45d3359c10ba1ec2d886a
Berkaycan AKKOÇ <77438994+berkayakkocc@users.noreply.github.com>
2023-04-17 12:52:57 +0300

    Categories API Endpoint

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
@@ -16,7 +17,17 @@ namespace NLayer.API.Controllers
             _categoryService = categoryService;
             _mapper = mapper;
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
 
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoriesDto));
+
+        }
         // api/categories/GetSingleCategoryByIdWithProducts/2
         [HttpGet("[action]/{categoryId}")]
 
